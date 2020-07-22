62de956fe50429a3ed23f9cbc3c39b0792a4aabf
1andyn <contact@ngandy.me>
2020-07-22 12:24:09 -1000

    Cleaned up misc code

--- src/views/IndexSections/Alerts.js
+++ src/views/IndexSections/Alerts.js
@@ -27,12 +27,12 @@ class Alerts extends React.Component {
         <h3 className="mt-lg mb-4">
           <span>Alerts</span>
         </h3>
-        <UncontrolledAlert color="success" fade={false}>
+        <UncontrolledAlert color="success" fade={true}>
           <span className="alert-inner--icon">
             <i className="ni ni-like-2" />
           </span>
           <span className="alert-inner--text ml-1">
-            <strong>Success!</strong> This is a success alert—check it out!
+            <strong>Success!</strong> Your application was restarted!
           </span>
         </UncontrolledAlert>
         <UncontrolledAlert color="info" fade={false}>
