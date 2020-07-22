c9ef6d2da11da2c7d7ec669d184470af558b2f06
1andyn <andy.work.dev@gmail.com>
2020-07-22 12:23:23 -1000

    Updated footer to point to source of Argon UI

--- src/components/Footers/AppFooter.js
+++ src/components/Footers/AppFooter.js
@@ -61,10 +61,10 @@ class AppFooter extends React.Component {
                 <Nav className="nav-footer justify-content-end">
                   <NavItem>
                     <NavLink
-                      href="https://www.creative-tim.com/"
+                      href="https://demos.creative-tim.com/argon-design-system/docs/getting-started/overview.html"
                       target="_blank"
                     >
-                    Creative Tim
+                    Argon Design System
                     </NavLink>
                   </NavItem>
                 </Nav>
@@ -76,30 +76,5 @@ class AppFooter extends React.Component {
     );
   }
 }
-
-/*
-            <Row>
-              <Col className="mb-5 mb-md-0" md="6">
-                <Card className="card-lift--hover shadow border-0">
-                  <Link to="/landing-page">
-                    <CardImg
-                      alt="..."
-                      src={require("assets/img/theme/landing.jpg")}
-                    />
-                  </Link>
-                </Card>
-              </Col>
-              <Col className="mb-5 mb-lg-0" md="6">
-                <Card className="card-lift--hover shadow border-0">
-                  <Link to="/profile-page">
-                    <CardImg
-                      alt="..."
-                      src={require("assets/img/theme/profile.jpg")}
-                    />
-                  </Link>
-                </Card>
-              </Col>
-            </Row>
-*/
 
 export default AppFooter;
