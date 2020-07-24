79095f854ab9ba746b32190a5f2ce093d072ed00
1andyn <contact@ngandy.me>
2020-07-23 18:35:55 -1000

    Removed unused navbar components

--- src/components/Navbars/AppNavbar.js
+++ src/components/Navbars/AppNavbar.js
@@ -16,17 +16,12 @@
 
 */
 import React from "react";
-import { Link } from "react-router-dom";
 // JavaScript plugin that hides or shows a component based on your scroll
 import Headroom from "headroom.js";
 // reactstrap components
 import {
   Button,
   UncontrolledCollapse,
-  DropdownMenu,
-  DropdownItem,
-  DropdownToggle,
-  UncontrolledDropdown,
   UncontrolledTooltip,
   NavbarBrand,
   Navbar,
