98d5fb464014343187f6248fdcb9567e24c994a1
1andyn <contact@ngandy.me>
2020-07-24 00:56:36 -1000

    Rebuilt files after changing homepage in Package JSON

---
+++ precache-manifest.fb7c3bf521b8351883a5952cccc3b385.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751fcbf4e55bbfb49ea700b85e73c6a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6068e5c3e0aa09a09fa6",
+    "url": "./static/css/main.4f28a05a.chunk.css"
+  },
+  {
+    "revision": "bf77d52df2979b3757ce",
+    "url": "./static/js/2.82a4eb29.chunk.js"
+  },
+  {
+    "revision": "123c36955d3b020d1348e93b9ab88993",
+    "url": "./static/js/2.82a4eb29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6068e5c3e0aa09a09fa6",
+    "url": "./static/js/main.b0548e57.chunk.js"
+  },
+  {
+    "revision": "f9b2f982c21a4a77275818d9d6aa91cd",
+    "url": "./static/js/main.b0548e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a467cf09ebeffdfbf26",
+    "url": "./static/js/runtime-main.b8a8ff5d.js"
+  },
+  {
+    "revision": "b6bf11b4c4845e08dc2c6f2b5fdd5862",
+    "url": "./static/media/arg-react-sch.b6bf11b4.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "./static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "./static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "./static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "./static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "./static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "1db896be446a01157b5ca0e41602ad7a",
+    "url": "./static/media/system_seal.1db896be.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.679bf230400b9788aa5a0c66cf92ba7c.js"
+  "./precache-manifest.fb7c3bf521b8351883a5952cccc3b385.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
